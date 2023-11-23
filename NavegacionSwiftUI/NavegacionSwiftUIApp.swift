0b7292afb73345dcf252d5433ee3878543d88b55
Hazel Alain <hazelo235@gmail.com>
2023-11-23 15:46:26 -0600

    Initial Commit

---
+++ NavegacionSwiftUI/NavegacionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavegacionSwiftUIApp.swift
+//  NavegacionSwiftUI
+//
+//  Created by Hazel Alain on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NavegacionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
